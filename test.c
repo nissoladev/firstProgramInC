d3195208a47cc7a310900597970ebcc31f8254b7
Guilherme Nissola <nissoladev@gmail.com>
2025-05-20 21:37:18 -0300

    Novos recursos aplicados:
    Usos distintos de fgets
    
    Inserção de funcionalidade para o usuário digitar a data de nascimento e ser exibido no console
    
    Criação de calculadora básica

--- test.c
+++ test.c
@@ -2,8 +2,49 @@
 
 int main() {
     int age;
+    double gpa;
+    char grade;
+    char name[25];
+    int day, month, year;
+    double num1, num2;
+    float soma;
+    /*
     printf("Enter your age: \n");
     scanf("%d", &age);
-    printf("You are %d years old.\n", age);
+    printf("You are %d years old.\n\n", age);
+
+    printf("Enter your gpa \n");
+    scanf("%lf", &gpa);
+    printf("Your gpa is: %f\n\n", gpa);
+
+    printf("Enter your grade: \n");
+    scanf(" %c", &grade);
+    printf("Your grade is: %c\n\n", grade);
+
+
+    printf("Enter your name: \n");
+    fgets(name, 25, stdin);
+    printf("Your name is: %s", name);
+
+    printf("Enter your name: \n");
+    scanf("%s", name);
+    printf("Your name is: %s", name);
+
+    printf("\nEnter your name: \n");
+    fgets(name, 25, stdin);
+    printf("Your name is: %s", name);
+
+    printf("Enter your date birth in the format: (yyyy/mm/dd)\n");
+    scanf("%d/%d/%d", &year, &month, &day);
+    printf("Your birthday date is: %d/%d/%d\n", year, month, day);
+    */
+
+    printf("Basic Calculator...\n");
+    printf("Enter first number:\n");
+    scanf("%lf", &num1);
+    printf("Enter second number:\n");
+    scanf("%lf", &num2);
+    soma = (num1 + num2);
+    printf("Answer: %f", soma);
     return 0;
 }
